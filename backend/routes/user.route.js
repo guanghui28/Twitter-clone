a7e0d6a332a7e16f56c225252122777bd312d69b
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-05-08 21:07:38 +0700

    1-user routes and controller added

---
+++ backend/routes/user.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+	getUserProfile,
+	followUnfollowUser,
+	getSuggestUsers,
+	updateUser,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestUsers);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
+
+export default router;
